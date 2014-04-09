05876db7683727da4a292eeaa1fe2bb31dc1497e
Sam-Serpoosh <sam.serpoosh@gmail.com>
2014-04-08 21:05:31 -0400

    Take the average of similar users ratings for movie as predicted rating for user

--- recommender/movie_rating_predictor.rb
+++ recommender/movie_rating_predictor.rb
@@ -1,17 +1,15 @@
 require_relative "./cluster_engine"
 
-class RatingPredictor
+class MovieRatingPredictor
   def initialize
     @user_ratings_dir = "../sample_netflix_dataset/user_ratings/"
-    @users_ids_file   = "../sample_netflix_dataset/users"
-    @movies_dir       = "../sample_netflix_dataset/movies/"
   end
 
-  def predict(user, movie)
+  def predict_movie_rate_for_user(user, movie)
     user_movie_ratings = movie_ratings_by_user(user)
     users_ratings = users_ratings_for_movie(movie)
     similar_users = get_similar_users_based_on(user, user_movie_ratings, users_ratings.keys)
-    similar_users
+    calculate_predicted_rating(users_ratings, similar_users)
   end
 
   def movie_ratings_by_user(user)
@@ -42,6 +40,12 @@ class RatingPredictor
     cluster_engine.similar_users
   end
 
+  def calculate_predicted_rating(users_ratings, similar_users)
+    sum = 0
+    similar_users.each { |user_id| sum += users_ratings[user_id] }
+    sum / similar_users.count.to_f
+  end
+
   def extract_user_id_and_rating_value(user_rating_file_and_rating_info)
     rating_filepath = user_rating_file_and_rating_info.split(":")[0]
     rate_value      = user_rating_file_and_rating_info.split(":")[2].to_i
@@ -52,6 +56,14 @@ class RatingPredictor
 end
 
 if __FILE__ == $0
-  print RatingPredictor.new.predict("1003353", "0008387")
+  print MovieRatingPredictor.new.predict_movie_rate_for_user("1003353", "0016139")
   puts
 end
+
+#0008387
+#0009049
+#0010042
+#0011283
+#0012084
+#0016139
+
