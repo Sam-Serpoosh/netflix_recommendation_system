8e7b303d2c0e29f2217b64f1a0272866cbb5e883
Sam-Serpoosh <sam.serpoosh@gmail.com>
2014-04-14 19:19:16 -0400

    Use mean of ALL movies rate by users not ONLY common movies between them
    - Move calculation of predicted rate to MovieRatingPredictor where it belongs

--- recommendation_engine/movie_rating_predictor.rb
+++ recommendation_engine/movie_rating_predictor.rb
@@ -4,17 +4,17 @@ require_relative "./pearson_correlation_coefficient_calculator"
 
 class MovieRatingPredictor
   def predict_movie_rate_for_user_by_pearson_correlation(user, movie)
-    user_movie_ratings = other_movie_ratings_by_user(user, movie)
-    users_ratings      = users_ratings_for_movie(movie)
-    pearson_calculator = PearsonCorrelationCoefficientCalculator.new(user, user_movie_ratings, users_ratings)
-    pearson_calculator.predict_rating
+    user_movie_ratings  = other_movie_ratings_by_user(user, movie)
+    other_users_ratings = users_ratings_for_movie(movie).reject { |other_user, rate| other_user == user }
+    correlation_weights = get_correlation_weight_with_other_users(user, user_movie_ratings, other_users_ratings)
+    calculate_predicted_rating_from_correlation(correlation_weights, user_movie_ratings, other_users_ratings)
   end
 
   def predict_movie_rate_for_user_by_clustering(user, movie)
     user_movie_ratings = other_movie_ratings_by_user(user, movie)
     users_ratings      = users_ratings_for_movie(movie)
     similar_users      = get_similar_users_based_on_ratings(user, user_movie_ratings, users_ratings.keys)
-    calculate_predicted_rating(users_ratings, similar_users)
+    calculate_predicted_rating_from_cluster(users_ratings, similar_users)
   end
 
   def other_movie_ratings_by_user(user, movie)
@@ -33,12 +33,33 @@ class MovieRatingPredictor
     users_ratings
   end
 
+  def get_correlation_weight_with_other_users(user, user_movie_ratings, other_users_ratings)
+    @pearson_calculator = PearsonCorrelationCoefficientCalculator.new(
+      user, user_movie_ratings, other_users_ratings)
+    @pearson_calculator.calculate_weight_with_other_users
+  end
+
+  def calculate_predicted_rating_from_correlation(weights, user_movie_ratings, other_users_ratings)
+    user_ratings_mean = @pearson_calculator.calculate_mean_of_rating(user_movie_ratings)
+    user_ratings_mean + weighted_sum_of_ratings(weights, other_users_ratings)
+  end
+
+  def weighted_sum_of_ratings(weights, other_users_ratings)
+    sum = 0
+    weights.each do |user, weight|
+      user_ratings_mean = @pearson_calculator.movie_ratings_mean_for_user(user)
+      sum += weight * (other_users_ratings[user] - user_ratings_mean)
+    end
+    total_weight = weights.values.inject(0) { |sum, w| sum += w }
+    sum / total_weight
+  end
+
   def get_similar_users_based_on_ratings(user, user_movie_ratings, other_users)
     cluster_engine = ClusterEngine.new(user, user_movie_ratings, other_users)
     cluster_engine.similar_users
   end
 
-  def calculate_predicted_rating(users_ratings, similar_users)
+  def calculate_predicted_rating_from_cluster(users_ratings, similar_users)
     sum = 0
     similar_users.each { |user_id| sum += users_ratings[user_id] }
     sum / similar_users.count.to_f
@@ -50,6 +71,6 @@ movies = ["0008387", "0009049", "0010042", "0011283", "0012084", "0016139"]
 if __FILE__ == $0
   movies.each do |movie|
     predictor = MovieRatingPredictor.new
-    puts predictor.predict_movie_rate_for_user_by_pearson_correlation("1003353", movie)
+    puts "#{movie} => #{predictor.predict_movie_rate_for_user_by_pearson_correlation("1003353", movie)}"
   end
 end
--- recommendation_engine/pearson_correlation_coefficient_calculator.rb
+++ recommendation_engine/pearson_correlation_coefficient_calculator.rb
@@ -2,23 +2,17 @@ class PearsonCorrelationCoefficientCalculator
   def initialize(user, user_movie_ratings, other_users_ratings_for_movie)
     @user = user
     @user_movie_ratings = user_movie_ratings 
-    @other_users_ratings_for_movie = other_users_ratings_for_movie.reject { |other_user, rate| other_user == @user }
+    @other_users_ratings_for_movie = other_users_ratings_for_movie
     @other_users = @other_users_ratings_for_movie.keys
     @other_users_ratings_means = {}
   end
 
-  def predict_rating
-    weights = calculate_weight_with_other_users
-    user_rating_mean = calculate_mean_of_rating(@user_movie_ratings)
-    predicted_rate = user_rating_mean
-    weights.each do |user, weight|
-      predicted_rate += weight * (@other_users_ratings_for_movie[user] - @other_users_ratings_means[user])
-    end
-    predicted_rate
+  def movie_ratings_mean_for_user(user) 
+    @other_users_ratings_means[user]
   end
 
   def calculate_weight_with_other_users
-    weights = {}
+    weights, @user_ratings_mean = {}, calculate_mean_of_rating(@user_movie_ratings)
     @other_users.each do |user|
       weights[user] = calculate_weight_with_user(user)
     end
@@ -26,30 +20,28 @@ class PearsonCorrelationCoefficientCalculator
   end
 
   def calculate_weight_with_user(user)
-    other_user_movie_ratings = get_movie_ratings_of_user(user)
-    common_movies = get_movies_rated_by_both_users(other_user_movie_ratings)
+    other_user_all_movie_ratings = get_movie_ratings_of_user(user)
+    common_movies = get_movies_rated_by_both_users(other_user_all_movie_ratings)
     user_movie_ratings, other_user_movie_ratings = get_common_movie_ratings(
-      common_movies, other_user_movie_ratings) 
-    @other_users_ratings_means[user] = calculate_mean_of_rating(other_user_movie_ratings)
-    numerator(user_movie_ratings, other_user_movie_ratings) / 
-      denominator(user_movie_ratings, other_user_movie_ratings)
+      common_movies, other_user_all_movie_ratings) 
+    @other_users_ratings_means[user] = calculate_mean_of_rating(other_user_all_movie_ratings)
+    numerator(user_movie_ratings, other_user_movie_ratings, user) / 
+      denominator(user_movie_ratings, other_user_movie_ratings, user)
   end
 
   def get_movie_ratings_of_user(user)
-    movies = @user_movie_ratings.keys
     movies_ratings = {}
-    movies.each do |movie|
-      movie_id, rate = DataExtractor.user_rating_for_movie(user, movie)
-      movies_ratings[movie_id] = rate
+    DataExtractor.movies_rated_by_user(user) do |movie, rate|
+      movies_ratings[movie] = rate
     end
     movies_ratings.reject { |movie, rate| movie.nil? }
   end
 
-  def get_movies_rated_by_both_users(other_user_movie_ratings)
+  def get_movies_rated_by_both_users(other_user_all_movie_ratings)
     common_movies = []
     @user_movie_ratings.each do |movie, rate|
-      next if rate == 0 || !other_user_movie_ratings.has_key?(movie) || 
-        other_user_movie_ratings[movie] == 0 
+      next if rate == 0 || !other_user_all_movie_ratings.has_key?(movie) || 
+        other_user_all_movie_ratings[movie] == 0 
       common_movies << movie
     end
     common_movies
@@ -65,22 +57,20 @@ class PearsonCorrelationCoefficientCalculator
     return user_movie_ratings, other_user_movie_ratings
   end
 
-  def numerator(user_movie_ratings, other_user_movie_ratings)
-    user_rating_mean = calculate_mean_of_rating(user_movie_ratings)
-    other_user_rating_mean = calculate_mean_of_rating(other_user_movie_ratings)
+  def numerator(user_movie_ratings, other_user_movie_ratings, other_user)
+    other_user_rating_mean = @other_users_ratings_means[other_user]
     sum = 0
     user_movie_ratings.each do |movie, rate|
-      sum += (rate - user_rating_mean) * (other_user_movie_ratings[movie] - other_user_rating_mean)
+      sum += (rate - @user_ratings_mean) * (other_user_movie_ratings[movie] - other_user_rating_mean)
     end
     sum
   end
 
-  def denominator(user_movie_ratings, other_user_movie_ratings)
-    user_rating_mean = calculate_mean_of_rating(user_movie_ratings)
-    other_user_rating_mean = calculate_mean_of_rating(other_user_movie_ratings)
+  def denominator(user_movie_ratings, other_user_movie_ratings, other_user)
+    other_user_rating_mean = @other_users_ratings_means[other_user]
     sum_squared_for_user, sum_squared_for_other_user = 0, 0
     user_movie_ratings.each do |movie, rate|
-      sum_squared_for_user       += (rate - user_rating_mean) ** 2
+      sum_squared_for_user       += (rate - @user_ratings_mean) ** 2
       sum_squared_for_other_user += (other_user_movie_ratings[movie] - other_user_rating_mean) ** 2
     end
     Math.sqrt(sum_squared_for_user * sum_squared_for_other_user)
